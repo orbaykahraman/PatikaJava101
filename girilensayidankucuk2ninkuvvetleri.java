497221f21528b892ec3ee2cc9165ab4ab973ce75
Orbay Kahraman <96729527+orbaykahraman@users.noreply.github.com>
2023-05-06 12:56:30 +0300

    Create girilensayidankucuk2ninkuvvetleri.java

---
+++ girilensayidankucuk2ninkuvvetleri.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class girilensayidankucuk2ninkuvvetleri {
+    public static void main(String[] args) {
+        Scanner girdi = new Scanner(System.in);
+        int x;
+        System.out.print("Sayı giriniz :");
+        x = girdi.nextInt();
+
+        System.out.println("4'ün kuvvetleri aşağıda sıralanmıştır");
+        for (int i = 1; i < x; i*=4){
+            System.out.println(i);
+        }
+        System.out.println("5'in kuvvetleri aşağıda sıralanmıştır");
+        for (int i = 1; i < x; i*=5){
+            System.out.println(i);
+        }
+
+    }
+}
